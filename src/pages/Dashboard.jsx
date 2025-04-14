9a8dd4bf9979d86f08da51847f18c27603bb73a4
abduvohid-jck <abduvohidmurodov26@gmail.com>
2025-04-14 15:14:54 +0500

    Complated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { removeToken } from "../redux/authSlice";
 function Dashboard() {
-  return <div>Dashboard</div>;
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
+  function Logout() {
+    dispatch(removeToken());
+    navigate("/");
+  }
+  return (
+    <div>
+      Dashboard
+      <button
+        onClick={Logout}
+        className="rounded-[10px] p-[10px] bg-[#3a90ff] m-[10px] text-white"
+      >
+        Log out
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { removeToken } from "../redux/authSlice";
 
 function Profile() {
-  return <div>Profile</div>;
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
+  function Logout() {
+    dispatch(removeToken());
+    navigate("/");
+  }
+  return (
+    <div>
+      Profile
+      <button
+        onClick={Logout}
+        className="rounded-[10px] p-[10px] bg-[#3a90ff] m-[10px] text-white"
+      >
+        Log out
+      </button>
+    </div>
+  );
 }
 
 export default Profile;
